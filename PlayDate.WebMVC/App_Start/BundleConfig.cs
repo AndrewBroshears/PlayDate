dac998d28febefc53932e5160de7eb53a81750d3
Andrew Broshears <ambroshears@gmail.com>
2022-04-19 20:36:46 -0500

    UIUpdates

--- PlayDate.WebMVC/App_Start/BundleConfig.cs
+++ PlayDate.WebMVC/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace PlayDate.WebMVC
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-darkly.css",
                       "~/Content/site.css"));
         }
     }
